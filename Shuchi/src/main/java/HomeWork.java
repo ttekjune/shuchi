01e37d4c4a2051702f3b59de7d22c2d113037cb9
Shu-S6i7 <Shu-S6i7@DESKTOP-AS72BD9>
2018-10-01 19:53:03 -0400

    Homework 2

--- Shuchi/src/main/java/HomeWork.java
+++ Shuchi/src/main/java/HomeWork.java
@@ -1,4 +1,4 @@
-
+  
 public class HomeWork {
 
 	void logingTest () {
@@ -9,12 +9,20 @@ public class HomeWork {
 		System.out.println("I submitted Homework");
 	}
 	
+	int sum () {
+	int a=2; 
+	int b=23;
+	int sum = a+b;
+	return sum;
+	
+	}
 	public static void main(String[] args) {
 		
 		HomeWork.logingTest1();
 		
 		HomeWork obj = new HomeWork ();
 		obj.logingTest();
+		
 		
 	}
 
---
+++ Shuchi/src/main/java/com/Homework/Oct12018.java
@@ -0,0 +1,26 @@
+/*I.  Write a variable country = united states of America and print in console
+  II. Write code to find the odd number from 1 to 1000*/
+
+
+
+package com.Homework;
+
+public class Oct12018 {
+
+	public static void main(String[] args) {
+		String Country = "United State of America";
+		System.out.println(Country);
+		
+		System.out.println("These are the odd numbers: ");
+		for (int a= 1; a<= 1000; a++) {
+			//System.out.println(a);
+			if (a%2==0) {
+				System.out.print( "");			
+			}
+		else {
+			System.out.println(a);
+	}
+
+}
+	}
+}
